ecf093f2014b1fa63ff6c3b9004e08f3f2e051e5
Jordan Irwin <antumdeluge@gmail.com>
2021-05-14 19:34:26 -0700

    Set or unset "owner" attribute in node meta data when wand is used on it

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 ## Ownit: Node Ownership
 
+### Description:
+
+A tool for setting or removing ownership of a node. Ownership is determined by the "owner" attribute in the node's meta data. In order for this to actually be effective, other mods must respect that attribute. Otherwise, setting the owner does nothing.
+
+One instance where this tool is useful is in cases where a player wants to unown a door so others can use it freely.
+
+In future versions, setting ownership will only be possible in protected areas where the player using the wand is the owner.
+
 ### Usage:
 
-Use the wand to own or unown a node. To be able to set yourself as owner, node must be in a protected area owned by you.
+Simply use the wand on a node to own or unown it. Nodes already owned by other players cannot be hijacked.
 
 ### Licensing:
 
--- wand.lua
+++ wand.lua
@@ -4,4 +4,32 @@ core.register_craftitem(ownit.modname .. ":wand", {
 	short_description = "Ownit Wand",
 	inventory_image = "ownit_wand.png",
 	stack_max = 1,
+	on_use = function(itemstack, user, pointed_thing)
+		if not user:is_player() then return end
+		if pointed_thing.type ~= "node" then return end
+		local pos = core.get_pointed_thing_position(pointed_thing, false)
+		local node = core.get_node_or_nil(pos)
+		if not node then return end
+
+		local pname = user:get_player_name()
+		local nmeta = core.get_meta(pos)
+		local current_owner = nmeta:get_string("owner")
+
+		if current_owner ~= "" and pname ~= current_owner then
+			core.chat_send_player("You cannot take ownership of a node owned by another player")
+			return
+		end
+
+		local unown = false
+		if pname == current_owner then unown = true end
+
+		if unown then
+			nmeta:set_string("owner", nil)
+			core.chat_send_player(pname, "You no longer own this node")
+		else
+			-- FIXME: should only be able to set ownership of nodes in protected areas owned by same player
+			nmeta:set_string("owner", pname)
+			core.chat_send_player(pname, "You now own this node")
+		end
+	end,
 })
