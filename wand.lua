8a842c4dcf192ac0cf3323d2e2320237c004c9df
Jordan Irwin <antumdeluge@gmail.com>
2021-05-15 15:51:35 -0700

    Add support for simple_protection mod

--- wand.lua
+++ wand.lua
@@ -5,11 +5,21 @@ local function is_area_owner(pos, pname)
 	if not pname then return false end
 
 	if use_s_protect then
-		-- FIXME: not working
-		return s_protect.can_access(pos, pname)
+		local claim = s_protect.get_claim(pos)
+		if claim then return pname == claim.owner end
 	else
-		core.is_protected(pos, pname)
+		return core.is_protected(pos, pname)
 	end
+
+	return false
+end
+
+local function check_node_pos(target)
+		if target.type ~= "node" then return end
+		local pos = core.get_pointed_thing_position(target, false)
+		if not core.get_node_or_nil(pos) then return end
+
+		return pos
 end
 
 
@@ -20,28 +30,25 @@ core.register_craftitem(ownit.modname .. ":wand", {
 	stack_max = 1,
 	on_use = function(itemstack, user, pointed_thing)
 		if not user:is_player() then return end
-		if pointed_thing.type ~= "node" then return end
-		local pos = core.get_pointed_thing_position(pointed_thing, false)
-		local node = core.get_node_or_nil(pos)
-		if not node then return end
+		local pos = check_node_pos(pointed_thing)
+		if not pos then return end
 
 		local pname = user:get_player_name()
 		local nmeta = core.get_meta(pos)
-		local current_owner = nmeta:get_string("owner")
+		local node_owner = nmeta:get_string("owner")
 
-		if current_owner ~= "" and pname ~= current_owner then
-			core.chat_send_player(pname, "You cannot take ownership of a node owned by another player")
+		if node_owner ~= "" and pname ~= node_owner then
+			core.chat_send_player(pname, "You cannot take ownership of a node owned by " .. node_owner)
 			return
 		end
 
 		local unown = false
-		if pname == current_owner then unown = true end
+		if pname == node_owner then unown = true end
 
 		if unown then
 			nmeta:set_string("owner", nil)
 			core.chat_send_player(pname, "You no longer own this node")
 		else
-			--[[
 			if not is_area_owner(pos, pname) then
 				core.chat_send_player(pname, "You cannot take ownership of a node that is not in an area owned by you")
 				return
@@ -49,9 +56,7 @@ core.register_craftitem(ownit.modname .. ":wand", {
 
 			nmeta:set_string("owner", pname)
 			core.chat_send_player(pname, "You now own this node")
-			]]
 
-			core.chat_send_player(pname, "Setting ownership currently not functional")
 		end
 	end,
 })
